3f7e251693aabd9d70ba78b0890aeda4ab509ce8
Alexey Gerasimov <alexger204@gmail.com>
2021-01-31 22:33:37 +0500

    refactor

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,7 @@ class BasePageLocators():
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn")
     BASKET_LINK_INVALID = (By.CSS_SELECTOR, ".basket-mini a.btn-invalid")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
@@ -20,6 +21,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_FIELD_REPEAT = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
 
 
 class ProductPageLocators():
--- pages/login_page.py
+++ pages/login_page.py
@@ -18,3 +18,18 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(
             *LoginPageLocators.REGISTER_FORM), "Register form is not present"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        password_field = self.browser.find_element(
+            *LoginPageLocators.PASSWORD_FIELD)
+        password_field_repeat = self.browser.find_element(
+            *LoginPageLocators.PASSWORD_FIELD_REPEAT)
+        register_button = self.browser.find_element(
+            *LoginPageLocators.REGISTER_BUTTON)
+
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        password_field_repeat.send_keys(password)
+
+        register_button.click()
